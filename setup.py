889ea256be04d42612b3c3fc4a60b0bdf55174d8
mirusu400 <mirusu400@naver.com>
2026-04-05 23:02:56 +0900

    update: Add github workflow for auto publishing into pypi

--- setup.py
+++ setup.py
@@ -2,55 +2,66 @@ import pybind11, sys
 from os import path
 from glob import glob
 from setuptools import setup
-from pybind11.setup_helpers import Pybind11Extension, build_ext, ParallelCompile, naive_recompile
+from pybind11.setup_helpers import (
+    Pybind11Extension,
+    build_ext,
+    ParallelCompile,
+    naive_recompile,
+)
 
-__version__ = "0.0.2"
+__version__ = "0.1.0"
 
 ParallelCompile("NPY_NUM_BUILD_JOBS", needs_recompile=naive_recompile).install()
 
 # read the contents of your README file
 this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
-        
+
 ext_modules = [
     Pybind11Extension(
-        'external_proc._external_proc',
+        "external_proc._external_proc",
         sorted(glob("src/external_proc/*.cpp")),
-        define_macros = [('VERSION_INFO', __version__)],
+        define_macros=[("VERSION_INFO", __version__)],
         include_dirs=[pybind11.get_include()],
-        language='c++',
-        extra_compile_args=['/std:c++17'],
-        extra_link_args=['imagehlp.lib', 'Advapi32.lib']
+        language="c++",
+        extra_compile_args=["/std:c++17"],
+        extra_link_args=["imagehlp.lib", "Advapi32.lib"],
     ),
 ]
 
 setup(
-    name='external_proc',  # имя библиотеки собранной pybind11
+    name="external_proc",  # имя библиотеки собранной pybind11
     version=__version__,
-    author='bananasss00',
-    author_email='bananasss00@yandex.com',
-    description='pybind11 extension',
+    author="bananasss00",
+    author_email="bananasss00@yandex.com",
+    description="pybind11 extension",
     long_description=long_description,
-    long_description_content_type='text/markdown',
+    long_description_content_type="text/markdown",
     ext_modules=ext_modules,
-    requires=['pybind11'],  # Указываем зависимость от pybind11
+    requires=["pybind11"],  # Указываем зависимость от pybind11
     # package_dir = {'': 'lib'},
-    packages=['external_proc'],
-    python_requires='>=3.6,<4.0',
+    packages=["external_proc"],
+    python_requires=">=3.6,<4.0",
     classifiers=[
-        'Operating System :: Microsoft :: Windows :: Windows 10 :: Windows 11',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
-        'Programming Language :: Python :: 3.12',
-        'Programming Language :: Python :: 3.13',
+        "Operating System :: Microsoft :: Windows",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
     ],
-    package_data={ 
-        'external_proc': ["__init__.py", "_external_proc.pyi", "external_proc.py", "get_proc_x32.exe", "get_proc_x64.exe", ],
+    package_data={
+        "external_proc": [
+            "__init__.py",
+            "_external_proc.pyi",
+            "external_proc.py",
+            "get_proc_x32.exe",
+            "get_proc_x64.exe",
+        ],
     },
     # cmdclass={"build_ext": build_ext}, # Pybind11Extension::extra_compile_args=['/std:c++17'],
     zip_safe=False,
